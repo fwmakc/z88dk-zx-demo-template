cbf577e5cfc97d3cd31d8b72e94ce4e978eff949
fwmakc <fwmakc@mail.ru>
2024-11-25 18:36:29 +0300

    update

---
+++ run.cmd
@@ -0,0 +1,10 @@
+@echo off
+
+cd %~dp0
+if errorlevel 1 exit /B 1
+
+call compile.cmd
+if errorlevel 1 exit /B 1
+
+tools\speccy\speccy bin\demo.sna
+if errorlevel 1 exit /B 1
---
+++ src/sprites.h
@@ -0,0 +1,13 @@
+#ifndef SPRITES_H
+#define SPRITES_H
+
+#include "sprites/box.h"
+
+unsigned char* boxSpriteState[4] = {
+    boxPhase0,
+    boxPhase1,
+    boxPhase2,
+    boxPhase3
+};
+
+#endif
